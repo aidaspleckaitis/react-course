a79213383ecf25a448c20ac5cd8269e5eeeacf74
Aidas <stepiuvejas@gmail.com>
2018-09-17 21:32:53 +0300

    created tic tack toe game

--- tic-tack/src/App.js
+++ tic-tack/src/App.js
@@ -1,12 +1,113 @@
 import React from 'react';
-import './App.css';
+import 'reset-css';
+import './styles/index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi Jhon!</h1>
-    </div>
-  );
+const WINNING_COMBOS = [
+  // row combos
+  [[0, 0], [0, 1], [0, 2]],
+  [[1, 0], [1, 1], [1, 2]],
+  [[2, 0], [2, 1], [2, 2]],
+
+  // columns combos
+  [[0, 0], [1, 0], [2, 0]],
+  [[0, 1], [1, 1], [2, 1]],
+  [[0, 2], [1, 2], [2, 2]],
+
+  // diagonale
+  [[0, 0], [1, 1], [2, 2]],
+  [[0, 2], [1, 1], [2, 0]],
+];
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    const initial = [...Array(3)].map(() => [...Array(3)]);
+
+    this.state = {
+      gameData: initial,
+      turn: 0,
+      players: { one: { winner: false, icon: "🤡" }, two: { winner: false, icon: "👺" } }
+    }
+
+  };
+
+  isWinner = (newGameData, player) => {
+
+    return WINNING_COMBOS.some(combo => combo.every(([row, box]) => newGameData[row][box] === player.icon));
+
+  };
+
+  onClick = ({ row, box }) => {
+
+    const { gameData, turn, players } = this.state;
+
+    const playerId = turn % 2 === 0 ? "one" : "two";
+
+    const currentPlayer = players[playerId];
+
+    if (!gameData[row][box]) {
+
+      gameData[row][box] = currentPlayer.icon;
+      this.setState({ gameData, turn: turn + 1 });
+      console.log(this.isWinner(gameData, currentPlayer));
+
+    }
+
+    if (this.isWinner(gameData, currentPlayer)) {
+
+      players[playerId].winner = true;
+      this.setState({ players });
+
+    }
+
+  };
+
+  findWinner = () => {
+
+    const { players } = this.state;
+
+    return Object.values(players).find(player => player.winner === true);
+
+  };
+
+  renderWinner = () => {
+
+    const winner = this.findWinner();
+
+    return (
+      <div>
+        <p>Winner is: {winner.icon}</p>
+      </div>
+    );
+
+  }
+
+  renderGame = () => {
+
+    const { gameData } = this.state;
+
+    return gameData.map((rowData, i) => (
+      <div className="row" key={i}>
+        {rowData.map((boxData, boxIndex) => (
+          <div role="button" className="box" key={boxIndex} onClick={() => this.onClick({ row: i, box: boxIndex })}>
+            {boxData}
+          </div>
+        ))
+        }
+      </div>
+    ))
+
+  };
+
+  render() {
+
+    const winner = this.findWinner();
+
+    return winner ? this.renderWinner() : this.renderGame();
+
+  }
 }
 
 export default App;
