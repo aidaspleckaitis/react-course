f066f3545cb26d3f226797838c28065d019a6673
Aidas <aidas.pleckaitis@gmail.com>
2018-10-16 21:28:25 +0300

    tested middleware

--- food-shop-class/src/app/state/index.js
+++ food-shop-class/src/app/state/index.js
@@ -1,6 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const mid = ({ getState, dispatch }) => next => action => {
+  console.log('Middleware.');
+  return next(action);
+};
+
+const store = createStore(reducers, applyMiddleware(mid));
 
 export default store;
